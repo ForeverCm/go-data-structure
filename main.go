43ef5f81390659e0f3b3db35e5de25398bb8814b
chenmeng <chenmeng@qutoutiao.net>
2020-07-23 10:50:55 +0800

    test

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
 
 func main() {
 	fmt.Println("hello world")
-	//var target, source string
-	//fmt.Scan(&target, &source)
-	//fmt.Println(subSequenceIndex(target, source))
-}
-
-func init() {
-	fmt.Println("init--------")
 }
